0b784a20011bcac1a3c746f00869e5610c0f1650
brian <bho123@ufl.edu>
2019-09-02 00:17:51 -0400

    modified specs

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.tipcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText mealCost;
+    EditText tipAmt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    Integer cost, tips;
+
+    public void tip(View view){
+
     }
 }
